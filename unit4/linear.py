da475ffdcb814dea874b3d88fd61f14dcdb9b67d
https://github.com/Namgaylham/bia101 <03230168.gcbs@rub.edu.bt>
2024-03-21 02:07:27 -0700

    ff

---
+++ unit4/linear.py
@@ -0,0 +1,17 @@
+#searching
+#sorting
+
+#!problem1
+#?input
+user_input = [1,2,3,4,5,6,7,8,9,11]
+# check to see if a certain number exist in the user input array
+n= 14
+#linear search
+result = False # variable to keep track of your answer
+for e in user_input:
+    if e==n:
+        print("exist")
+    else:
+        print("not found")
+
+#if else, for loops, print, calculation(+,-)
