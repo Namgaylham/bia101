4515df77936a4f58c3f6532a91e1feefdd94f2d4
Namgaylham <03230168.gcbs@rub.edu.bt>
2024-03-23 07:59:41 -0700

    check user age for vote

--- checkvote.py
+++ checkvote.py
@@ -6,22 +6,15 @@
 # ?if else statement
 # ?if above 18, print "You can vote"
 # ?if below 18, print "You canot vote"
-
-userInput = input('Please type your age: ')
-userage = int(userInput)
-
-print('The type of user input is:', userInputType)
-if user can vote
+#if user can vote
+userAge  = input()
 if userAge >= 18:
-    print('You can vote')
+    print('You can vote')
 else:
-    print('You cannot vote')
+    print('You cannot vote')
 
 # 3. Check if user can vote
 if userAge > 18:
-    print('You can vote')
+    print('You can vote')
 elif userAge < 18:
- print('You cannot vote')
-
-userInputNumber = float(userInput)
-print('The type of userInputNumber is:', type(userInputNumber))    
+    print('You cannot vote')
--- session1.py
+++
@@ -1,5 +0,0 @@
-print('hello world')
-print("hello class")
-
-print("i am namgay")
-print("i am namgay")
