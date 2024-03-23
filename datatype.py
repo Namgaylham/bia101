e3f0ab7ad24651792f08b5ce526bbc1b4dd25b73
Namgaylham <03230168.gcbs@rub.edu.bt>
2024-03-23 07:38:19 -0700

    changed datatype work

--- datatype.py
+++ datatype.py
@@ -1,10 +1,6 @@
-variable1 = 10
-varible2 = 11
-# like x=10 in mathematics
-
-name = 'dechen'
-name2 = 'dechen2'
-score = 10.1
-didhewin = True
-print(variable1)
-print(type(variable1))
+varaible1 = 10 #integer
+name = "dechen"#string
+name2 = "dechen2"
+score = 10.1#float/decimal
+didhewin = True# boolean
+print(varaible1)
