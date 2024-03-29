82d2df899f217fb9a7007b0bcc8ebd6cfac78fe2
Namgaylham <03230168.gcbs@rub.edu.bt>
2024-03-28 23:19:28 -0700

    contains duplicate solution

---
+++ unit4/containsduplicate.py
@@ -0,0 +1,18 @@
+input_arr = [1,2,3,4]
+
+def containsDuplicate(nums):
+    # solution code here
+    for element in nums:
+        for i in nums:
+            print('element:',element)
+            print('i:', i)
+            print('=========')
+
+
+    return True    
+#either return true or false
+
+  
+result = containsDuplicate(input_arr)
+
+print(result)
